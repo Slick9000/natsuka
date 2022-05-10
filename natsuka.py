bd691e1074de7aa7b1ad30e95b4db00a9622f8d8
Slick9000 <37941291+Slick9000@users.noreply.github.com>
2022-05-10 17:35:39 -0400

    separate functions, multi song downloading support

--- natsuka.py
+++ natsuka.py
@@ -1,49 +1,115 @@
-#!/usr/bin/env python3
-
 import aiohttp
 import asyncio
 import calendar
-import re
+from datetime import timedelta
 import mutagen
+import re
+import time
 
 
-print("natsuka - wip spotify downloader\n"
-      "Thanks to JoshuaDoes for making this possible.\n"
-     )
-
-option = input("""
-What would you like to do today?
-
-Option 1 - Download a single track
-Option 2 - Download Album          (Doesn't currently work)
-Option 3 - Download Playlist       (Doesn't currently work)
-
-: """
-              )
-
-while not any(x in option for x in ["1", "2", "3"]):
-
-    print("Invalid option!")
+async def main():
+    print("natsuka - wip spotify downloader\n"
+          "Thanks to JoshuaDoes for making this possible."
+          )
 
     option = input("""
 What would you like to do today?
 
 Option 1 - Download a single track
-Option 2 - Download Album          (Doesn't currently work)
-Option 3 - Download Playlist       (Doesn't currently work)
+Option 2 - Download multiple tracks
+Option 3 - Download Album              (Doesn't currently work)
+Option 4 - Download Playlist           (Doesn't currently work)
+Option 5 - Exit
 
 : """
                )
 
-if option == "1":
+    while not any(x in option for x in ["1", "2", "3", "4", "5"]):
+
+        print("Invalid option!")
+
+        option = input("""
+What would you like to do today?
+
+Option 1 - Download a single track
+Option 2 - Download multiple tracks
+Option 3 - Download Album              (Doesn't currently work)
+Option 4 - Download Playlist           (Doesn't currently work)
+Option 5 - Exit
+
+: """
+               )
+
+    if option == "1":
+
+        await singleTrackProcess()
+
+        try:
+        
+            input("Press enter to continue.\n")
+        
+        except SyntaxError:
+        
+            pass
+        
+        await main()
+
+    if option == "2":
+
+        await multiTrackProcess()
+
+        try:
+        
+            input("Press enter to continue.\n")
+        
+        except SyntaxError:
+        
+            pass
+        
+        await main()
+
+    if option == "3":
+
+        print("Currently unimplemented.\n")
+
+        try:
+        
+            input("Press enter to continue.\n")
+        
+        except SyntaxError:
+        
+            pass
+        
+        await main()
+
+    if option == "4":
+
+        print("Currently unimplemented.\n")
+
+        try:
+        
+            input("Press enter to continue.\n")
+        
+        except SyntaxError:
+        
+            pass
+        
+        await main()
+
+    if option == "5":
+
+        exit()
+
     
-    UID = input("Enter the track's URL\n:")
+async def singleTrackProcess():
+
+    UID = input("Enter the track's URL\n: ")
 
     while len(re.findall(r'(https?://[^\s]+)', UID)) == 0:
 
         print("Invalid URL!")
 
-        UID = input("Enter the track's URL\n:")
+        UID = input("Enter the track's URL\n: ")
 
     query = "?"
     
@@ -60,14 +126,131 @@ if option == "1":
         group = UID.split("/")
 
         UID = group[len(group) - 1]
+
+
+    async with aiohttp.ClientSession() as session:
+
+        async with session.get(f"https://music.joshuadoes.com/track/spotify:track:{UID}?pass=pleasesparemyendpoints&stream&quality=2") as trackJSON:
+
+                trackData = await trackJSON.json()
+                    
+                trackName = trackData['name']
+
+                trackAlbum = trackData['album']['name']
+
+                trackNumber = trackData['number']
+
+                artistName = trackData['album']['artist'][0]['name']
+
+                albumRelease = trackData['album']['date']
+
+                comment = f"{trackData['album']['label']}."
+
+                print(f"Track Name: {trackName}")
+
+                print(f"Album Name: {trackAlbum}")
+
+                print(f"Artist Name: {artistName}")
+
+                print(f"Album Release: {calendar.month_name[albumRelease['month']]} {albumRelease['day']}, {albumRelease['year']}")
+
+        async with session.get(f"https://music.joshuadoes.com/v1/stream/spotify:track:{UID}?pass=pleasesparemyendpoints&stream&quality=2") as audioData:
+
+                fileName = re.sub('[\/:*?"<>|]', '', trackName)
+
+                with open(f"{fileName}.ogg", "wb") as fd:
+
+                    while True:
+
+                        chunk = await audioData.content.read()
+
+                        if not chunk:
+
+                            break
+
+                        fd.write(chunk)
+
+                        print("Song Downloaded!")
+
+                        meta = mutagen.File(fd.name)
+
+                        if meta.tags is None:
+
+                            meta.tags = mutagen.id3.ID3()
+
+                        meta['title'] = trackName
+                            
+                        meta['album'] = trackAlbum
+                            
+                        meta['tracknumber'] = str(trackNumber)
+                            
+                        meta['artist'] = artistName
+
+                        meta['comment'] = comment
+
+                        meta['year'] = str(albumRelease['year'])
+
+                        try:
+
+                            meta.save()
+
+                        #due to a bug in mutagen, an error always occurs here
+                        #although the data writes to the file just fine. mutagen pls fix
+                        except:
+
+                            print("Metadata Applied!")
+
+                            await session.close()
+
+async def multiTrackProcess():
+
+    songList = []
+
+    UID = ""
+
+    while UID != "START":
+
+        UID = input(f"Enter songs (Current number of songs: {len(songList)}\n"
+                    "Type 'START' to begin download.\n: "
+                    )
+
+        while len(re.findall(r'(https?://[^\s]+)', UID)) == 0 and UID != "START":
+
+            print("Invalid URL!")
+
+            UID = input("Enter the track's URL\n:")
+
+        query = "?"
     
-    async def main():
+        if query in UID:
+        
+            UID = UID.split(query)
+
+            group = UID[0].split("/")
+
+            UID = group[len(group) - 1]
+        
+        else:
+
+            group = UID.split("/")
+
+            UID = group[len(group) - 1]
+
+        if UID != "START":
+
+            songList.append(UID)
+
+    start_time = time.monotonic()
+
+    print(f"Loading {len(songList)} songs...\n\n")
+
+    for i in songList:
 
         async with aiohttp.ClientSession() as session:
 
-            async with session.get(f"https://music.joshuadoes.com/track/spotify:track:{UID}?pass=pleasesparemyendpoints&stream&quality=2") as resp:
+            async with session.get(f"https://music.joshuadoes.com/track/spotify:track:{i}?pass=pleasesparemyendpoints&stream&quality=2") as trackJSON:
 
-                    trackData = await resp.json()
+                    trackData = await trackJSON.json()
                     
                     trackName = trackData['name']
 
@@ -89,7 +272,7 @@ if option == "1":
 
                     print(f"Album Release: {calendar.month_name[albumRelease['month']]} {albumRelease['day']}, {albumRelease['year']}")
 
-            async with session.get(f"https://music.joshuadoes.com/v1/stream/spotify:track:{UID}?pass=pleasesparemyendpoints&stream&quality=2") as resp:
+            async with session.get(f"https://music.joshuadoes.com/v1/stream/spotify:track:{i}?pass=pleasesparemyendpoints&stream&quality=2") as audioData:
 
                     fileName = re.sub('[\/:*?"<>|]', '', trackName)
 
@@ -97,7 +280,7 @@ if option == "1":
 
                         while True:
 
-                            chunk = await resp.content.read()
+                            chunk = await audioData.content.read()
 
                             if not chunk:
 
@@ -124,24 +307,22 @@ if option == "1":
                             meta['comment'] = comment
 
                             meta['year'] = str(albumRelease['year'])
-
+    
                             try:
 
                                 meta.save()
 
                             #due to a bug in mutagen, an error always occurs here
-                            #although the data writes file. mutagen pls fix
+                            #although the data writes to the file just fine. mutagen pls fix
                             except:
 
-                                print("Metadata Applied!")
-    
-if option == "2":
+                                print("Metadata Applied!\n\n")
 
-    pass
+                                await session.close()
+        
+    end_time = time.monotonic()
 
-if option == "3":
-
-    pass
+    print(f"Download time: {timedelta(seconds=end_time - start_time)}\n")
 
 if __name__ == "__main__":
 
