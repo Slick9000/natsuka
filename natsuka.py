0e16e468285420fb416f3a1cbea496e628e6ac16
Slick9000 <37941291+Slick9000@users.noreply.github.com>
2022-05-10 14:49:47 -0400

    spring cleaning!

--- natsuka.py
+++ natsuka.py
@@ -7,11 +7,9 @@ import re
 import mutagen
 
 
-print("""
-natsuka - wip spotify downloader and possibly music stream application
-
-Thanks to JoshuaDoes for making this possible."""
-      )
+print("natsuka - wip spotify downloader\n"
+      "Thanks to JoshuaDoes for making this possible.\n"
+     )
 
 option = input("""
 What would you like to do today?
@@ -19,18 +17,33 @@ What would you like to do today?
 Option 1 - Download a single track
 Option 2 - Download Album          (Doesn't currently work)
 Option 3 - Download Playlist       (Doesn't currently work)
-Option 4 - Stream music            (Doesn't currently work)
 
-:"""
+: """
+              )
+
+while not any(x in option for x in ["1", "2", "3"]):
+
+    print("Invalid option!")
+
+    option = input("""
+What would you like to do today?
+
+Option 1 - Download a single track
+Option 2 - Download Album          (Doesn't currently work)
+Option 3 - Download Playlist       (Doesn't currently work)
+
+: """
                )
 
 if option == "1":
     
-    UID = input("""
-Enter the track's URL
+    UID = input("Enter the track's URL\n:")
 
-:"""
-          )
+    while len(re.findall(r'(https?://[^\s]+)', UID)) == 0:
+
+        print("Invalid URL!")
+
+        UID = input("Enter the track's URL\n:")
 
     query = "?"
     
@@ -38,31 +51,24 @@ Enter the track's URL
         
         UID = UID.split(query)
 
-        list = UID[0].split("/")
+        group = UID[0].split("/")
 
-        id = list[len(list) - 1]
-
-        UID = id
-
-        print(UID)    
+        UID = group[len(group) - 1]
         
     else:
 
-        list = UID.split("/")
+        group = UID.split("/")
 
-        id = list[len(list) - 1]
-      
-        UID = id
-    
+        UID = group[len(group) - 1]
     
     async def main():
 
         async with aiohttp.ClientSession() as session:
 
-            async with session.get(f"http://joshuadoes.com:8080/track/{UID}?pass=hFUhqM9n") as resp:
+            async with session.get(f"https://music.joshuadoes.com/track/spotify:track:{UID}?pass=pleasesparemyendpoints&stream&quality=2") as resp:
 
                     trackData = await resp.json()
-
+                    
                     trackName = trackData['name']
 
                     trackAlbum = trackData['album']['name']
@@ -73,6 +79,8 @@ Enter the track's URL
 
                     albumRelease = trackData['album']['date']
 
+                    comment = f"{trackData['album']['label']}."
+
                     print(f"Track Name: {trackName}")
 
                     print(f"Album Name: {trackAlbum}")
@@ -81,8 +89,7 @@ Enter the track's URL
 
                     print(f"Album Release: {calendar.month_name[albumRelease['month']]} {albumRelease['day']}, {albumRelease['year']}")
 
-
-            async with session.get(f"http://joshuadoes.com:8080/download/{UID}?pass=hFUhqM9n") as resp:
+            async with session.get(f"https://music.joshuadoes.com/v1/stream/spotify:track:{UID}?pass=pleasesparemyendpoints&stream&quality=2") as resp:
 
                     fileName = re.sub('[\/:*?"<>|]', '', trackName)
 
@@ -98,24 +105,35 @@ Enter the track's URL
 
                             fd.write(chunk)
 
-                            file = mutagen.File(f"{fileName}.ogg")
+                            print("Song Downloaded!")
 
-                            file['title'] = trackName
-                            
-                            file['album'] = trackAlbum
-                            
-                            file['tracknumber'] = str(trackNumber)
-                            
-                            file['artist'] = artistName
+                            meta = mutagen.File(fd.name)
 
-                            file['year'] = str(albumRelease['year'])
+                            if meta.tags is None:
 
-                            file.save()
+                                meta.tags = mutagen.id3.ID3()
+
+                            meta['title'] = trackName
                             
-                    
-    loop = asyncio.get_event_loop()
-    
-    loop.run_until_complete(main())
+                            meta['album'] = trackAlbum
+                            
+                            meta['tracknumber'] = str(trackNumber)
+                            
+                            meta['artist'] = artistName
+
+                            meta['comment'] = comment
+
+                            meta['year'] = str(albumRelease['year'])
+
+                            try:
+
+                                meta.save()
+
+                            #due to a bug in mutagen, an error always occurs here
+                            #although the data writes file. mutagen pls fix
+                            except:
+
+                                print("Metadata Applied!")
     
 if option == "2":
 
@@ -125,6 +143,6 @@ if option == "3":
 
     pass
 
-else:
-    
-    pass
+if __name__ == "__main__":
+
+    asyncio.run(main())
